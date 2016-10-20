ca2c2e8a69a8cbef4bd53a8ec2f77e9591b1160d
김민우 <Bamdule5@gmail.com>
2016-10-20 17:31:12 +0900

    jvadd

--- test/src/main/java/com/kedu/board/HomeController.java
+++ test/src/main/java/com/kedu/board/HomeController.java
@@ -24,7 +24,7 @@ public class HomeController {
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
+		logger.info("안녕하세요 정식이형! {}.", locale);
 		
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
