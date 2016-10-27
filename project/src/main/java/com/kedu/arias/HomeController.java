25b09af98265fbb4264371da54a942abc3de5753
김민우 <Bamdule5@gmail.com>
2016-10-27 11:26:53 +0900

    20161027

--- project/src/main/java/com/kedu/arias/HomeController.java
+++ project/src/main/java/com/kedu/arias/HomeController.java
@@ -24,7 +24,7 @@ public class HomeController {
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
-		logger.info("정식이형 저도 되요 ㅋ"{}.", locale);
+
 		
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
