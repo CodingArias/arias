3603a1276c904f0e3c6a86a8f961ee16fe4fb787
dev_jsp <sonjs2001@naver.com>
2016-11-02 18:43:49 +0900

    20161102 손정식

--- project/src/main/java/com/kedu/arias/product/controller/ProductSearchController.java
+++ project/src/main/java/com/kedu/arias/product/controller/ProductSearchController.java
@@ -1,28 +1,17 @@
-package com.kedu.arias;
+package com.kedu.arias.product.controller;
 
 import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-/**
- * Handles requests for the application home page.
- */
 @Controller
-public class HomeController {
-	
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
-	@RequestMapping(value = "/", method = RequestMethod.GET)
+public class ProductSearchController {
+	@RequestMapping(value = "/search", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		
 		Date date = new Date();
@@ -31,9 +20,6 @@ public class HomeController {
 		model.addAttribute("serverTime", formattedDate );
 		
 		
-		return "home";
+		return "product/product_search";
 	}
-	
-	
-	
 }
