180c3ed1823ceb542679b9a7bbe89ad3cf91819e
유현수 <yjiny88@gmail.com>
2016-11-03 16:39:51 +0900

    notice dto

---
+++ project/src/main/java/com/kedu/arias/notice/dto/NoticeDto.java
@@ -0,0 +1,56 @@
+package com.kedu.arias.notice.dto;
+
+import java.util.Date;
+
+public class NoticeDto {
+	
+	private Integer notice_seq;
+	private String member_id;
+	private String notice_title;
+	private String notice_content;
+	private Date notice_regdate;
+	private int notice_count;
+	public Integer getNotice_seq() {
+		return notice_seq;
+	}
+	public void setNotice_seq(Integer notice_seq) {
+		this.notice_seq = notice_seq;
+	}
+	public String getMember_id() {
+		return member_id;
+	}
+	public void setMember_id(String member_id) {
+		this.member_id = member_id;
+	}
+	public String getNotice_title() {
+		return notice_title;
+	}
+	public void setNotice_title(String notice_title) {
+		this.notice_title = notice_title;
+	}
+	public String getNotice_content() {
+		return notice_content;
+	}
+	public void setNotice_content(String notice_content) {
+		this.notice_content = notice_content;
+	}
+	public Date getNotice_regdate() {
+		return notice_regdate;
+	}
+	public void setNotice_regdate(Date notice_regdate) {
+		this.notice_regdate = notice_regdate;
+	}
+	public int getNotice_count() {
+		return notice_count;
+	}
+	public void setNotice_count(int notice_count) {
+		this.notice_count = notice_count;
+	}
+	@Override
+	public String toString() {
+		return "NoticeDto [notice_seq=" + notice_seq + ", member_id=" + member_id + ", notice_title=" + notice_title
+				+ ", notice_content=" + notice_content + ", notice_regdate=" + notice_regdate + ", notice_count="
+				+ notice_count + "]";
+	}
+
+}
